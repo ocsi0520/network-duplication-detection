a6f2d58ce911834305e9a46ff421e8ee712fca4c
Faragó Tamás <ocsi0520@gmail.com>
2025-07-11 15:06:44 +0200

    fix missing linking of libraries

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "counter.hpp"
+#include "TypeName.hpp"
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    std::cout << count_letter_a("Hello World from main") << std::endl;
+    std::cout << get_type_name(count_letter_a("Hello World from main")) << std::endl;
     return 0;
 }
