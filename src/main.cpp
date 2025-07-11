ba8ba9d13c83cafad25545d06bc67c687a90d141
Faragó Tamás <ocsi0520@gmail.com>
2025-07-11 21:56:24 +0200

    invoke Reader in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
-#include "counter.hpp"
-#include "TypeName.hpp"
+#include "StreamReader.hpp"
+#include <fstream>
+#include <iostream>
 
 int main()
 {
-    std::cout << count_letter_a("Hello World from main") << std::endl;
-    std::cout << get_type_name(count_letter_a("Hello World from main")) << std::endl;
+    std::ifstream file{"data/network_utf8.csv"};
+    if (!file)
+    {
+        std::cout << "Could not open file" << std::endl;
+        return -1;
+    }
+    MyNetwork::CellParser cell_parser;
+    MyNetwork::LineParser line_parser{cell_parser};
+    MyNetwork::DataStreamReader reader{file, line_parser};
+
+    auto data = reader.read_data();
+    std::cout << "read data size: " << data.size() << std::endl;
+
     return 0;
 }
