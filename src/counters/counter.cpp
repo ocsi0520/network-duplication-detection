9a6a97133a6668a06cf3358802b0aff2640318c6
Faragó Tamás <ocsi0520@gmail.com>
2025-07-12 11:28:15 +0200

    remove dummy codes

--- src/counters/counter.cpp
+++
@@ -1,6 +0,0 @@
-#include <algorithm>
-#include <string>
-
-int count_letter_a(const std::string& a_str) {
-    return std::count(a_str.begin(), a_str.end(), 'a');
-}
--- src/counters/counter.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <string>
-
-int count_letter_a(const std::string& a_str);
