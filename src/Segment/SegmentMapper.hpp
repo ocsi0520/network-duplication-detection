ac6a833391cf544b151455fcc9ca945fd2fae4dc
Faragó Tamás <ocsi0520@gmail.com>
2025-07-13 13:08:32 +0200

    add SegmentMapper

---
+++ src/Segment/SegmentMapper.hpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include "NetworkSegment.hpp"
+
+namespace MyNetwork
+{
+    class SegmentMapper
+    {
+    public:
+        std::vector<Segment> map_from_record(const NetworkRecord &);
+    };
+}
