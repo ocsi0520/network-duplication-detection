a5f6ff3e1ecb6e5926017443f959387c5b2355ea
Faragó Tamás <ocsi0520@gmail.com>
2025-07-16 16:27:00 +0200

    add UniqueListFactory

---
+++ src/DuplicationDetection/LocationBased/UniqueList/UniqueListFactory.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "SegmentMerger.hpp"
+#include "UniqueList.hpp"
+
+namespace MyNetwork
+{
+    class UniqueListFactory
+    {
+    public:
+        // TODO: check this
+        UniqueListFactory(const SegmentMerger &sm = SegmentMerger{}) : merger{sm} {};
+        UniqueList create_unique_list()
+        {
+            return UniqueList{merger};
+        }
+
+    private:
+        SegmentMerger merger;
+    };
+}
