c10d5d3848fdea489ec9d045feda509b3215f41b
Faragó Tamás <ocsi0520@gmail.com>
2025-07-20 22:21:04 +0200

    add gmock library to tests

--- src/test_helpers.cmake
+++ src/test_helpers.cmake
@@ -1,13 +1,13 @@
 include(FetchContent)
 FetchContent_Declare(
-  googletest
-  URL https://github.com/google/googletest/archive/refs/tags/v1.14.0.zip
+    googletest
+    URL https://github.com/google/googletest/archive/refs/tags/v1.14.0.zip
 )
 FetchContent_MakeAvailable(googletest)
 
-function (get_all_test_files_from a_module_dir result)
-file(GLOB_RECURSE ALL_CPP_FILES CONFIGURE_DEPENDS
-    "${a_module_dir}/*.cpp"
+function(get_all_test_files_from a_module_dir result)
+    file(GLOB_RECURSE ALL_CPP_FILES CONFIGURE_DEPENDS
+        "${a_module_dir}/*.cpp"
     )
 
     set(TEST_SOURCES "")
@@ -21,7 +21,7 @@ file(GLOB_RECURSE ALL_CPP_FILES CONFIGURE_DEPENDS
     set(${result} ${TEST_SOURCES} PARENT_SCOPE)
 endfunction()
 
-function (get_test_helper_files_from a_module_dir result)
+function(get_test_helper_files_from a_module_dir result)
     get_all_test_files_from(${a_module_dir} EXTRA_TEST_SOURCES)
 
     # Exclude files ending with `_tests.cpp`
@@ -35,10 +35,10 @@ function (get_test_helper_files_from a_module_dir result)
 endfunction()
 
 function(register_module_tests module_name module_dir)
-    set(optional_extra_test_deps ${ARGV2})  # get the 3rd argument manually (if provided)
+    set(optional_extra_test_deps ${ARGV2}) # get the 3rd argument manually (if provided)
 
     get_all_test_files_from(${module_dir} TEST_SOURCES)
-    
+
     if(optional_extra_test_deps)
         get_test_helper_files_from(${optional_extra_test_deps} EXTRA_TEST_SOURCES)
         list(APPEND TEST_SOURCES ${EXTRA_TEST_SOURCES})
@@ -46,7 +46,7 @@ function(register_module_tests module_name module_dir)
 
     # debug
     # message(${module_dir})
-    
+
     # if(${module_dir} MATCHES ".*src/DuplicationDetection$")
     #     message("!!!!!!! ${module_dir} \n")
     #     foreach(file_path ${TEST_SOURCES})
@@ -60,8 +60,9 @@ function(register_module_tests module_name module_dir)
         add_executable(${target_name} ${TEST_SOURCES})
         target_link_libraries(${target_name}
             PRIVATE
-                ${module_name}
-                GTest::gtest_main
+            ${module_name}
+            GTest::gtest_main
+            GTest::gmock_main
         )
         add_test(NAME ${target_name} COMMAND ${target_name})
         set_tests_properties(${target_name} PROPERTIES LABELS "${module_name}")
